8db86cc59ab9b84e481b779f0121a65ce9938939
ljm350582528 <350582528@qq.com>
2018-03-13 11:57:35 +0800

    1

--- js/index.js
+++ js/index.js
@@ -95,7 +95,6 @@ let flag=true
 {
     function content(parent) {
         const types = parent.querySelectorAll(".teshuzi");
-        console.log(types)
         const goods = parent.querySelectorAll(".dapei_tu");
         types.forEach(function (ele, index) {
             ele.onmouseenter = function () {
@@ -118,5 +117,91 @@ let flag=true
     // content(contentList[4]);
     contentList.forEach(function (ele) {
         content(ele);
+    })
+}
+
+{function wheel(parent) {
+    let prev=parent.querySelector(".btnleft");
+    let next=parent.querySelector(".btnright");
+    let inner=parent.querySelector(".dahezi");
+    let contentss=parent.querySelectorAll(".nrong");
+    console.log(contentss)
+    let pagers=parent.querySelectorAll(".lunbodian li");
+    let n=0;
+    next.onclick=function () {
+        n++;
+        if (n===contentss.length){
+            n=contentss.length-1;
+            return;
+        }
+        inner.style.marginLeft=-296*n+"px";
+        pagers[n].classList.add("active3");
+        pagers[n-1].classList.remove("active3");
+        let obj=pagers[0];
+    };
+    prev.onclick=function () {
+        n--;
+        if(n===-1){
+            n=0;
+            return;
+        }
+        inner.style.marginLeft=-296*n+"px";
+        pagers[n].classList.add("active3");
+        pagers[n+1].classList.remove("active3");
+        let obj=pagers[0];
+    };
+    let obj=pagers[0];
+    pagers.forEach(function (ele,index) {
+        ele.onclick=function () {
+            obj.classList.remove("active3");
+            this.classList.add("active3");
+            obj=this;
+            inner.style.Left=index*-296*n+"px";
+            n=index;
+        }
+    })}
+    let items=document.querySelectorAll(".bigbox")
+    wheel(items[0]);
+    wheel(items[1]);
+    wheel(items[2]);
+    wheel(items[3]);
+}
+{
+    let nav=document.querySelector(".banner_nav");
+    let labels=document.querySelectorAll(".label");
+    let menus=document.querySelectorAll(".bannerzhankai");
+    let obj=menus[0];
+    labels.forEach(function (ele,index) {
+        ele.onmouseenter=function () {
+            obj.style.display="none";
+            menus[index].style.display="block";
+            obj=menus[index];
+        }
+        ele.onmouseleave=function () {
+            menus[index].style.display="none"
+        }
+    })
+}
+{
+    let labels=document.querySelectorAll(".daohang_title");
+    let menus=document.querySelectorAll(".daohang_nrong");
+    let word=document.querySelector(".daohang_box");
+    // let obj=menus[0];
+    console.log(labels)
+    labels.forEach(function (ele,index) {
+        ele.onmouseenter=function () {
+
+            word.style.height=400+"px";
+            // obj.style.display="none";
+            // word.addEventListener("transitionend",function () {
+                menus[index].style.display="block";
+            // });
+
+            // obj=menus[index];
+        }
+        ele.onmouseleave=function () {
+            word.style.height=0;
+                menus[index].style.display="none";
+        }
     })
 }
